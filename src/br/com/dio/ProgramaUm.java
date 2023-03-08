3cd70738eb5a4ee84748f9d931db89d7e7c83b82
PAB1610 <pabloanast1@gmail.com>
2023-03-08 15:39:56 -0300

    Commit 2

--- src/br/com/dio/ProgramaUm.java
+++ src/br/com/dio/ProgramaUm.java
@@ -1,17 +1,21 @@
 package br.com.dio;
 
+import java.util.Scanner;
+
 public class ProgramaUm {
 
 	public static void main(String[] args) {
 
 		Gato gato = new Gato(); 
-
+		
 		/*/ TODO Auto-generated method stub
 		// esboço de metodo criado automaticamente
 		int a = 50;
 		int b = 100;*/
-
+		 
 		System.out.println(gato);
 	}
+
+	
 
 }
